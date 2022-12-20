40fa7513171cce8043e90c68e3804214b8424452
Stan <8138029+squiig@users.noreply.github.com>
2022-12-20 22:17:12 +0100

    Added project files

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public GameObject rpsItemPrefab, containerObject;
+	public Sprite rockSprite, paperSprite, scissorsSprite;
+	public int paperSpawnAmt, rockSpawnAmt, scissorsSpawnAmt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		InitialSpawn();
+    }
+
+	public void InitialSpawn()
+	{
+		for (int i = 0; i < rockSpawnAmt; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height));
+			SpawnRPSItem(RPSType.Rock, pos);
+		}
+		for (int i = 0; i < paperSpawnAmt; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height));
+			SpawnRPSItem(RPSType.Paper, pos);
+		}
+		for (int i = 0; i < scissorsSpawnAmt; i++)
+		{
+			Vector3 pos = new Vector3(Random.Range(0, Screen.width), Random.Range(0, Screen.height));
+			SpawnRPSItem(RPSType.Scissors, pos);
+		}
+	}
+
+	public RPSItem SpawnRPSItem(RPSType type, Vector3 screenPos)
+	{
+		Vector3 pos = Camera.main.ScreenToWorldPoint(screenPos);
+		pos.z = 0;
+		RPSItem item = Instantiate(rpsItemPrefab, pos, Quaternion.identity).GetComponent<RPSItem>();
+		switch (type)
+		{
+			case RPSType.Rock:
+				item.SetType(RPSType.Rock);
+				item.gameObject.name = "rock" + item.GetHashCode();
+				return item;
+			case RPSType.Paper:
+				item.SetType(RPSType.Paper);
+				item.gameObject.name = "paper" + item.GetHashCode();
+				return item;
+			case RPSType.Scissors:
+				item.SetType(RPSType.Scissors);
+				item.gameObject.name = "scissors" + item.GetHashCode();
+				return item;
+			default:
+				return null;
+		}
+	}
+}
---
+++ Assets/Scripts/RPSItem.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public enum RPSType
+{
+	Rock,
+	Paper,
+	Scissors
+}
+
+public class RPSItem : MonoBehaviour
+{
+	public float speed;
+	public float targetRotationSpeed;
+	public RPSType type;
+
+	private GameManager _gameManager;
+	private Camera _cam;
+	private SpriteRenderer _spriteRenderer;
+	private Rigidbody2D _rb;
+	private Vector3 _direction;
+	private Vector3 _targetDirection;
+
+    void Awake()
+    {
+		_gameManager = FindObjectOfType<GameManager>();
+		_cam = Camera.main;
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_rb = GetComponent<Rigidbody2D>();
+    }
+
+	private void Start()
+	{
+		_direction = Vector3.up;
+		_targetDirection = Random.insideUnitCircle;
+	}
+
+	void Update()
+    {
+		//float speed = this.speed * Random.Range(-1f, 1f);
+		//Vector3 tpos = Random.insideUnitCircle * speed * Time.deltaTime;
+		//transform.Translate(tpos);
+
+		// Recalculate rot anchor sometimes
+		if (Random.value < .05f)
+		{
+			_targetDirection = Random.insideUnitCircle;
+		}
+
+		_direction = Vector3.RotateTowards(_direction, _targetDirection, targetRotationSpeed * Time.deltaTime, 0f);
+		transform.Translate(_direction * speed * Time.deltaTime);
+
+		// Draw a ray pointing at our target
+		Debug.DrawRay(transform.position, _targetDirection.normalized, Color.red);
+		Debug.DrawRay(transform.position, _direction.normalized, Color.blue);
+
+		// Correct border pos
+		Vector3 maxBounds = _cam.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+		Vector3 minBounds = _cam.ScreenToWorldPoint(Vector3.zero);
+		Vector3 pos = transform.position;
+		pos.x = Mathf.Clamp(pos.x, minBounds.x, maxBounds.x);
+		pos.y = Mathf.Clamp(pos.y, minBounds.y, maxBounds.y);
+		transform.position = pos;
+	}
+
+	private void OnTriggerEnter2D(Collider2D collider)
+	{
+		switch (collider.GetComponent<RPSItem>().type)
+		{
+			case RPSType.Rock:
+				if (type == RPSType.Scissors)
+					SetType(RPSType.Rock);
+				break;
+			case RPSType.Paper:
+				if (type == RPSType.Rock)
+					SetType(RPSType.Paper);
+				break;
+			case RPSType.Scissors:
+				if (type == RPSType.Paper)
+					SetType(RPSType.Scissors);
+				break;
+			default:
+				break;
+		}
+	}
+
+	public void SetType(RPSType type)
+	{
+		switch (type)
+		{
+			case RPSType.Rock:
+				_spriteRenderer.sprite = _gameManager.rockSprite;
+				this.type = RPSType.Rock;
+				break;
+			case RPSType.Paper:
+				_spriteRenderer.sprite = _gameManager.paperSprite;
+				this.type = RPSType.Paper;
+				break;
+			case RPSType.Scissors:
+				_spriteRenderer.sprite = _gameManager.scissorsSprite;
+				this.type = RPSType.Scissors;
+				break;
+			default:
+				break;
+		}
+	}
+}
